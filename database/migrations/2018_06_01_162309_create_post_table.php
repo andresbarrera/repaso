d6d92413f06e6c56f7e6e1edd5b39996ec0cb489
andresbarrera <andres.barrera@usach.cl>
2018-06-01 13:05:04 -0400

    Migracion de Posts

---
+++ database/migrations/2018_06_01_162309_create_post_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->increments('id_post');
+            $table->text('texto_post');
+            $table->dateTime('fecha_post');
+            $table->integer('fkid_user')->unsigned();
+            $table->foreign('fkid_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
